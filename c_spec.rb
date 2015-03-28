8871c859dff45480c0e7b78109fa89e79ec1353e
ka <ka.kaosf@gmail.com>
2015-03-29 01:19:50 +0900

    Add test for "sub"

--- c_spec.rb
+++ c_spec.rb
@@ -17,4 +17,20 @@ describe 'calc' do
       it { is_expected.to eq 1 }
     end
   end
+
+  describe 'sub' do
+    subject { sub(x, y) }
+
+    context 'x: 0, y: 0' do
+      let(:x) { 0 }
+      let(:y) { 0 }
+      it { is_expected.to eq 0 }
+    end
+
+    context 'x: 2, y: 1' do
+      let(:x) { 2 }
+      let(:y) { 1 }
+      it { is_expected.to eq 1 }
+    end
+  end
 end

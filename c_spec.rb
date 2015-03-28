de68549b637c713fce46c3a995317ade863489f9
ka <ka.kaosf@gmail.com>
2015-03-29 00:21:42 +0900

    Add test for "add"

--- c_spec.rb
+++ c_spec.rb
@@ -1,2 +1,20 @@
 require 'rspec'
 require './c'
+
+describe 'calc' do
+  describe 'add' do
+    subject { add(x, y) }
+
+    context 'x: 0, y: 0' do
+      let(:x) { 0 }
+      let(:y) { 0 }
+      it { is_expected.to eq 0 }
+    end
+
+    context 'x: 0, y: 1' do
+      let(:x) { 0 }
+      let(:y) { 1 }
+      it { is_expected.to eq 1 }
+    end
+  end
+end
